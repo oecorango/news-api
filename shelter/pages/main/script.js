1e78ce041532ec4229b37c1f8ee9d5e16f7529b2
unknown <oecorango@gmail.com>
2023-04-02 13:20:55 +0400

    feat: add adaptive size for pets page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,1 +1,24 @@
-console.log('Проверка верстки +7\n Верстка соответствует макету +35\n Требования css +6\n Интерактивность элементов +12\n Итого: по странице main 60\n \n Проверка перстки (pets) +7\n Верстка соответствует макету +15\n Требования css +4\n Интерактивность элементов +14\n Итого: по странице pets 40\n\n Итого: 100')
+// console.log('Проверка верстки +7\n Верстка соответствует макету +35\n Требования css +6\n Интерактивность элементов +12\n Итого: по странице main 60\n \n Проверка перстки (pets) +7\n Верстка соответствует макету +15\n Требования css +4\n Интерактивность элементов +14\n Итого: по странице pets 40\n\n Итого: 100')
+
+var runLengthEncoding = function(str) {
+	let obj = {}
+	const arr = [...str]
+	const res = [];
+	let acc = 0;
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i];
+		if (res.length === 0) {
+			res.push(element);
+			acc = acc + 1;
+		} else {
+			if (arr[i] === arr[i - 1]) {
+				acc = acc + 1;
+			}
+		}
+	}
+	console.log(acc)
+}
+
+runLengthEncoding("abc");
+
+runLengthEncoding("ccaaabbb"); 
