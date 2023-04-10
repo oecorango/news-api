2390e94b7264fbba1bda566533bbd2024dd0946b
unknown <oecorango@gmail.com>
2023-04-11 00:34:55 +0400

    feat: add style for modal window for size 1280

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -2,6 +2,7 @@ const pets = [
   {
     "name": "Jennifer",
     "img": "../../assets/image/pets/jennifer.png",
+		"img_modal": "../../assets/image/modal_pets/jennifer.png",
     "type": "Dog",
     "breed": "Labrador",
     "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
@@ -13,6 +14,7 @@ const pets = [
   {
     "name": "Sophia",
     "img": "../../assets/image/pets/sophia.png",
+		"img_modal": "../../assets/image/modal_pets/sophia.png",
     "type": "Dog",
     "breed": "Shih tzu",
     "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
@@ -24,6 +26,7 @@ const pets = [
   {
     "name": "Woody",
     "img": "../../assets/image/pets/woody.png",
+		"img_modal": "../../assets/image/modal_pets/woody.png",
     "type": "Dog",
     "breed": "Golden Retriever",
     "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
@@ -35,6 +38,7 @@ const pets = [
   {
     "name": "Scarlett",
     "img": "../../assets/image/pets/scarlett.png",
+		"img_modal": "../../assets/image/modal_pets/scarlett.png",
     "type": "Dog",
     "breed": "Jack Russell Terrier",
     "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
@@ -46,6 +50,7 @@ const pets = [
   {
     "name": "Katrine",
     "img": "../../assets/image/pets/katrine.png",
+		"img_modal": "../../assets/image/modal_pets/katrine.png",
     "type": "Cat",
     "breed": "British Shorthair",
     "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
@@ -57,6 +62,7 @@ const pets = [
   {
     "name": "Timmy",
     "img": "../../assets/image/pets/timmy.png",
+		"img_modal": "../../assets/image/modal_pets/timmy.png",
     "type": "Cat",
     "breed": "British Shorthair",
     "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
@@ -68,6 +74,7 @@ const pets = [
   {
     "name": "Freddie",
     "img": "../../assets/image/pets/freddie.png",
+		"img_modal": "../../assets/image/modal_pets/freddie.png",
     "type": "Cat",
     "breed": "British Shorthair",
     "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
@@ -79,6 +86,7 @@ const pets = [
   {
     "name": "Charly",
     "img": "../../assets/image/pets/charly.png",
+		"img_modal": "../../assets/image/modal_pets/charly.png",
     "type": "Dog",
     "breed": "Jack Russell Terrier",
     "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
@@ -288,3 +296,10 @@ BTN_PREV.addEventListener('click', movePrev);
 BTN_FIRST.addEventListener('click', moveFirst);
 BTN_LAST.addEventListener('click', moveLast)
 
+// ------------------------- pop-ap----------------------------
+
+const CARD = document.querySelectorAll('.card')
+
+CARD.addEventListener('click', (event) => {
+	console.log(event)
+})
