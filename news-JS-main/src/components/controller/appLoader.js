9d864b265dd9877c6f04587037a8802de5b378f3
Oleg Ershov <oecorango@gmail.com>
2023-06-06 03:27:38 +0400

    init: initial project setup (webpack, eslint, typescript)

--- news-JS-main/src/components/controller/appLoader.js
+++ news-JS-main/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '7cc0f9b1147148cea8501478d9c54264', // получите свой ключ https://newsapi.org/
         });
     }
 }
