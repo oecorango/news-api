fc7918281a49d42bb99f2f4195e5835b8da921a8
Oleg Ershov <oecorango@gmail.com>
2023-06-10 20:47:30 +0400

    feat: add news filter by name-channel

--- news-JS-main/src/components/find/find.ts
+++ news-JS-main/src/components/find/find.ts
@@ -1,1 +1,19 @@
-import './news.css';
+import './find.css';
+
+const input: HTMLButtonElement | null = document.querySelector('.find__input');
+const button = document.querySelector('.find__btn');
+
+button?.addEventListener('click', () => {
+  const newsChannel = document.querySelectorAll('.source__item');
+  if (input) {
+    const findValue: string = input.value.toLowerCase();
+
+    newsChannel.forEach((elem) => {
+      elem.classList.remove('source__item_off');
+      if (!elem.children[0].textContent?.toLowerCase().includes(findValue)) {
+        elem.classList.add('source__item_off');
+        elem.classList.add('source__item_off');
+      }
+    });
+  }
+});
--- news-JS-main/src/index.ts
+++ news-JS-main/src/index.ts
@@ -1,6 +1,6 @@
 import App from './components/app/app';
 import './global.css';
-import './components/find/find.css';
+import './components/find/find';
 import './components/footer/footer';
 
 const app = new App();
