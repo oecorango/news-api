12350b0d125c4c564832451c7ba5a1e75a92735f
Oleg Ershov <oecorango@gmail.com>
2023-06-11 00:43:45 +0400

    feat: change proxy for deploy task

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
-      apiKey: '740b9d1e99534ef79633a19976bca725', // получите свой ключ https://newsapi.org/  7cc0f9b1147148cea8501478d9c54264   |   740b9d1e99534ef79633a19976bca725
+    super('https://rss-news-api.onrender.com/', {
+      apiKey: '7cc0f9b1147148cea8501478d9c54264', // получите свой ключ https://newsapi.org/  7cc0f9b1147148cea8501478d9c54264   |   740b9d1e99534ef79633a19976bca725
     });
   }
 }
